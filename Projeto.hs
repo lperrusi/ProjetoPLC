19fd59e50eb8c1d30136250951bf1f042fce9c77
lucasbp <lucasbp@motorola.com>
2018-12-11 00:18:26 -0300

    Update MVar interativo

--- Projeto.hs
+++ Projeto.hs
@@ -12,6 +12,7 @@ threadAdv ropeCenter = do
 
 threadPlayer :: MVar Int -> IO()
 threadPlayer ropeCenter = do
+    --pega ação do jogador
     level <- getLine
     valueCenter <- takeMVar ropeCenter
     print valueCenter 
@@ -19,26 +20,20 @@ threadPlayer ropeCenter = do
 
     threadPlayer ropeCenter
 
+--verifica se a posição da corda chegou no limite
+
 threadCheck :: MVar Int -> IO()
 threadCheck ropeCenter = do
     valueCenter <- takeMVar ropeCenter
 
     if valueCenter >= 10 then do
-        putStrLn "PLAYER VENCEU!"
+        putStrLn "JOGADOR VENCEU!"
     else if valueCenter <= -10 then do
-        putStrLn "PLAYER PERDEU"
+        putStrLn "JOGADOR PERDEU"
     else do
         putMVar ropeCenter (valueCenter)
         threadCheck ropeCenter
         threadDelay 1000
-
-printRope :: Int -> Int -> [Char]
-printRope 0 0 = "X|-" ++ printRope 1 0
-printRope 0 center = "X-" ++ printRope 1 center
-printRope n center | n == center = "|" ++ printRope (n+1) center
-                   | n <= 20 = "-" ++ printRope (n+1) center
-                   | otherwise = "X"  
-
 
 createThreads :: Int -> (MVar Int -> IO()) -> MVar Int -> IO()
 createThreads n th mvar = do
@@ -49,26 +44,28 @@ createThreads n th mvar = do
     else
         threadDelay 1
     
-       
-
-   
-
 main :: IO()
 main = do
-
+--cria corda no 'centro'
     ropeCenter <- newMVar 0
 
     putStrLn "Escolha um nível - (1) (2) (3)"
+    
     level <- getLine
+    
     if level == "1" then do
         putStrLn "Nível Um Escolhido"
+
         createThreads 1 threadAdv ropeCenter
+    
     else if level == "2" then do
         putStrLn "Nível Dois Escolhido"
         createThreads 2 threadAdv ropeCenter
+    
     else if level == "3" then do
         putStrLn "Nível Tres Escolhido"
         createThreads 3 threadAdv ropeCenter
+    
     else do
         putStrLn "Escolha entre (1) (2) (3)"
         main
